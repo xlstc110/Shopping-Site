412f4fc1efaec8daff4388bedbbfaa0033467767
xlstc110 <xlstc110@gmail.com>
2023-08-31 04:15:19 -0700

    testing API gateway without keycloak

--- shopping-site/api-gateway/src/main/java/config/SecurityConfig.java
+++ shopping-site/api-gateway/src/main/java/config/SecurityConfig.java
@@ -6,26 +6,27 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity//enable web security for web-flux project, since the Spring Cloud Gateway project is based on Web Flux project, not Spring Web MVC
+//@Configuration
+//@EnableWebFluxSecurity//enable web security for web-flux project, since the Spring Cloud Gateway project is based on Web Flux project, not Spring Web MVC
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-
-        //first disable csrf() because we are only communicating the REST API through PostMan
-        //path matcher to the eureka uri so that when we are accessing the static resources, we do not need to send token with it.
-        //therefore, we need to exclude this call from our security configuration.
-        serverHttpSecurity.csrf()
-                .disable()
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-
-        //return the SecurityWebFilterChain object after the configuration
-        return serverHttpSecurity.build();
-    }
+//    @Bean
+//    //In Spring Security 6.1 both csrf() and jwt() are deprecated, so the code only works for version below 6.0
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+//
+//        //first disable csrf() because we are only communicating the REST API through PostMan
+//        //path matcher to the eureka uri so that when we are accessing the static resources, we do not need to send token with it.
+//        //therefore, we need to exclude this call from our security configuration.
+//        serverHttpSecurity.csrf()
+//                .disable()
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/eureka/**")
+//                        .permitAll()
+//                        .anyExchange()
+//                        .authenticated())
+//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//
+//        //return the SecurityWebFilterChain object after the configuration
+//        return serverHttpSecurity.build();
+//    }
 }
